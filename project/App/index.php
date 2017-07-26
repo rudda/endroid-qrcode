6d9ea6d4af42e78a0070c597afcfa5dba8874349
Rudda Beltrão <beltrao.rudah@gmail.com>
2017-07-26 16:08:49 -0300

    initial commit

---
+++ project/App/index.php
@@ -0,0 +1,9 @@
+<?php
+namespace Beltrao\App;
+/**
+ * Created by PhpStorm.
+ * User: Rudda Beltrao
+ * Date: 26/07/2017
+ * Time: 16:01
+ */
+
---
+++ project/qrcode/teste.php
@@ -0,0 +1,11 @@
+<?php
+namespace Beltrao\qrcode;
+
+/**
+ * Created by PhpStorm.
+ * User: Rudda Beltrao
+ * Date: 26/07/2017
+ * Time: 16:08
+ */
+
+
